2f1eedf6dfdd1670e83b8b4b0a138c933821aaf2
Nurullo <thenurullo@gmail.com>
2020-04-10 19:40:11 -0600

    added packages and classes matching our UML diagram

--- ENSF 409 Final project/src/server/DBManager.java
+++ ENSF 409 Final project/src/server/DBManager.java
@@ -1,3 +1,4 @@
+package server;
 import java.util.ArrayList;
 
 //This class is simulating a database for our
--- ENSF 409 Final project/src/server/Registration.java
+++ ENSF 409 Final project/src/server/Registration.java
@@ -1,3 +1,4 @@
+package server;
 
 public class Registration {
 	private Student theStudent;
