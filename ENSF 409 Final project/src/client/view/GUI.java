f1a9c63401818c417398c5c062108a3a0f4ebb57
Trevor Brown <trevorscottbrown@gmail.com>
2020-04-12 14:19:09 -0600

    Created a general idea for how mainframe will work, as well as how the frame control works for each frame in the controller package.

--- ENSF 409 Final project/src/client/view/GUI.java
+++ ENSF 409 Final project/src/client/view/GUI.java
@@ -2,4 +2,19 @@ package client.view;
 
 public class GUI {
 
+	public GUI() {
+		MainFrame mainframe = new MainFrame();
+		//other frames should be initialized inside the actionlisteners of other frame classes. 
+	}
+	
+	
+	//comment the main method once done. Just using for testing purposes right now. 
+	
+	public static void main(String[] args) {
+		
+		GUI g = new GUI();
+		
+	}
+	
+	
 }
