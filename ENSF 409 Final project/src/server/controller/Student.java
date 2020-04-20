a2adba6a133051a8fe6327024b99b0113b2fe9a0
Trevor Brown <trevorscottbrown@gmail.com>
2020-04-20 15:24:15 -0600

    Revert "Changed my boolean statement"
    This reverts commit ec6f45a392fa891f6a134e6211dcab15c5ed13e0.

--- ENSF 409 Final project/src/server/controller/Student.java
+++ ENSF 409 Final project/src/server/controller/Student.java
@@ -120,13 +120,9 @@ public class Student implements Serializable {
 	
 	public boolean notInCourseYet(Registration reg) {
 		
-		String regName = reg.getTheOffering().getTheCourse().getCourseName() + reg.getTheOffering().getTheCourse().getCourseNum();
-		
 		for (Registration r: studentRegList) {
 			
-			String name = r.getTheOffering().getTheCourse().getCourseName() + r.getTheOffering().getTheCourse().getCourseNum();
-			
-			if(regName.equals(name))
+			if(r.getTheOffering().getTheCourse().getCourseName().equals(reg.getTheOffering().getTheCourse().getCourseName())) {}
 			return true;
 			
 		}
